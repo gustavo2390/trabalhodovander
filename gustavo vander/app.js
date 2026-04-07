9d21c158f2bda9cfb3728ead30faf6c957561dd8
gustavo2390 <gustavo.santos.cavalcante@escola.pr.gov.br>
2026-04-07 08:07:16 -0300

    Add files via upload

--- gustavo vander/app.js
+++ gustavo vander/app.js
@@ -2,16 +2,29 @@ let inputTexto = document.querySelector(".texto");
 
 let idioma = document.querySelector(".idioma");
 
-let traducao = document.querySelector(".traducao");
+let traducaoSaida =document.querySelector(".traducao");
 
-async function traduzir() {
-    console.log(inputTexto.value);
-
-let url = "https://api.mymemory.translated.net/get?qbom%20trabalho!&langpair=pt|it";
+async function traduzir(){
+let url="https://api.mymemory.translated.net/get?q=" + inputTexto.value + "&langpair=pt|" + idioma.value;
 
 let resposta = await fetch(url);
-let traducao = await resposta.json();
 
-console.log(traducao.responseData.transLatedText);
+let traducao = await resposta.json();
+console.log(traducao);
+
+traducaoSaida.textContent=traducao.responseData.
+translatedText;
+}
+
+function microfone(){
+   // classe
+   let voz = window.SpeechRecognition || window.webkitSpeechRecognition;
+   // instanciando (construir)
+   let ouvirVoz = new voz();
+
+ouvirVoz.onresult = (evento) => {
+   inputTexto.value = evento.results[0][0].transcript;
+}
+ouvirVoz.start();
 
 }
